66eada01e09a32f86745ebb28a8a1fd0bac6e7a8
yiskaneto <44711170+yiskaneto@users.noreply.github.com>
2024-03-06 10:39:17 -0600

    user_data updated

--- modules/compute/auto_scaling_group_lb_training/03_launch_template.tf
+++ modules/compute/auto_scaling_group_lb_training/03_launch_template.tf
@@ -5,7 +5,7 @@ resource "aws_launch_template" "main" {
   image_id               = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.vpc_security_group_id]
-  user_data              = filebase64("${path.module}/${var.user_data}") # Also: base64decode("${path.module}/var.user_data")
+  user_data              = base64decode(var.user_data)
   iam_instance_profile {
     name = aws_iam_instance_profile.ec2_instance_profile.name
   }
