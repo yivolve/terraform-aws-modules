305cef5712489ab260d38146b5f5f1c8a4118011
yiskaneto <44711170+yiskaneto@users.noreply.github.com>
2024-03-08 09:21:38 -0600

    alb_sg_allow_all_outbound added

--- modules/compute/security_groups_lb_only_cloudfare_access/02-alb_sg.tf
+++ modules/compute/security_groups_lb_only_cloudfare_access/02-alb_sg.tf
@@ -62,3 +62,14 @@ resource "aws_security_group_rule" "alb_sg_allow_http_inbound" {
   cidr_blocks      = local.alb_ingress_cidr_ipv4
   ipv6_cidr_blocks = local.alb_ingress_ipv6_cidr_blocks
 }
+
+resource "aws_security_group_rule" "alb_sg_allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks      = local.alb_ingress_cidr_ipv4
+  ipv6_cidr_blocks = local.alb_ingress_ipv6_cidr_blocks
+}
