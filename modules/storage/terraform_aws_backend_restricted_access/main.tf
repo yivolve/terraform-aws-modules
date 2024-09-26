a14b42000be22924594d549625ddcb1bc07e0e3d
yiskaneto <44711170+yiskaneto@users.noreply.github.com>
2024-09-25 20:36:36 -0600

    server_side_encryption added

--- modules/storage/terraform_aws_backend_restricted_access/main.tf
+++ modules/storage/terraform_aws_backend_restricted_access/main.tf
@@ -90,6 +90,10 @@ resource "aws_dynamodb_table" "terraform_locks" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
+  server_side_encryption {
+   enabled = true
+  }
+
   attribute {
     name = "LockID"
     type = "S"
